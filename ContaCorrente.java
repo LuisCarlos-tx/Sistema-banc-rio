09cca5a091517ad9663f5d36a5b6ff0636eade54
Luis Carlos <luiscarlostvx34@gmail.com>
2025-10-09 22:20:40 -0300

    ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,4 +1,4 @@
-public class ContaCorrente extends Conta implements Tributavel {
+public class ContaCorrente extends Conta implements ITributavel {
     private static final double TAXA_SAQUE = 0.05;
     private static final double TAXA_TRIBUTO = 0.01;
 
@@ -9,10 +9,12 @@ public class ContaCorrente extends Conta implements Tributavel {
     @Override
     public boolean sacar(double valor) {
         double valorComTaxa = valor * (1 + TAXA_SAQUE);
-        if (saldo >= valorComTaxa) {
+        if (valor > 0 && saldo >= valorComTaxa) {
             saldo -= valorComTaxa;
+            System.out.println("Saque de R$ " + valor + " realizado. Taxa: R$ " + (valor * TAXA_SAQUE));
             return true;
         }
+        System.out.println("Saldo insuficiente para saque!");
         return false;
     }
 
@@ -20,6 +22,7 @@ public class ContaCorrente extends Conta implements Tributavel {
     public boolean transferir(double valor, Conta contaDestino) {
         if (sacar(valor)) {
             contaDestino.depositar(valor);
+            System.out.println("Transferência de R$ " + valor + " para conta " + contaDestino.getNumero() + " realizada!");
             return true;
         }
         return false;
@@ -30,6 +33,11 @@ public class ContaCorrente extends Conta implements Tributavel {
         return saldo * TAXA_TRIBUTO;
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + " | Tipo: Conta Corrente";
+    }
+}
     @Override
     public String toString() {
         return super.toString() + " | Tipo: Conta Corrente";
